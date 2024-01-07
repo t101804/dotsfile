875d43db70d50389924f9a66a0d4abc51d44788f
t101804 <t101804@protonmail.com>
2024-01-08 06:39:35 +0700

    adds addon

--- config/hypr/scripts/globalcontrol.sh
+++ config/hypr/scripts/globalcontrol.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # wallpaper var
-EnableWallDcol=0
+EnableWallDcol=1
 ConfDir="${XDG_CONFIG_HOME:-$HOME/.config}"
 CloneDir="/home/replican/Downloads/hyprdots"
 ThemeCtl="$ConfDir/hypr/theme.ctl"
